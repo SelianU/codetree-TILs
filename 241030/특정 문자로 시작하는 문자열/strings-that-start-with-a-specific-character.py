5cc66d4914a8bab23784a8d28d623f19adc63118
Jung-SW <88493935+Jung-SW@users.noreply.github.com>
2024-10-30 11:35:55 +0900

    Title: 특정 문자로 시작하는 문자열, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241030/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+li = [input() for _ in range(n)]
+
+c = input()
+
+count = 0
+gkq = 0
+
+for k in li:
+    if c in k:
+        count += 1
+        gkq += len(k)
+
+avg = gkq/count
+
+print(f"{count} {avg:.2f}")
