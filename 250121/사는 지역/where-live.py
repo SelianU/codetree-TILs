ad4b498b2e1a9c8d94f4f39679c8a303acf5f7a8
SelianU <Selian2341@gmail.com>
2025-01-21 07:38:01 +0900

    Title: 사는 지역, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250121/사는 지역/where-live.py
@@ -0,0 +1,34 @@
+n = int(input())
+name = []
+street_address = []
+region = []
+
+for _ in range(n):
+    n_i, s_i, r_i = input().split()
+    name.append(n_i)
+    street_address.append(s_i)
+    region.append(r_i)
+
+# Write your code here!
+class Street:
+    def __init__(self, name, street_address, region):
+        self.name = name
+        self.street_address = street_address
+        self.region = region
+    
+    def print(self):
+        print(f"name {self.name}\naddr {self.street_address}\ncity {self.region}")
+
+streets = []
+
+for i in range(n):
+    street = Street(name[i], street_address[i], region[i])
+    streets.append(street)
+
+slow_name = 0
+
+for i in range(1, n):
+    if streets[i - 1].name < streets[i].name:
+        slow_name = i
+
+streets[slow_name].print()
