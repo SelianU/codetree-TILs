17ef194b28ced02d286419c3a9d175d58c3178b4
SelianU <Selian2341@gmail.com>
2025-01-28 20:15:56 +0900

    Title: Date to Date, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250128/Date to Date/date-to-date.py
+++ 250128/Date to Date/date-to-date.py
@@ -3,15 +3,14 @@ m1, d1, m2, d2 = map(int, input().split())
 # Write your code here!
 months = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-first, second = d1, d2
+first, second = d1, d2 + 1
 
-for i in range(m1 + 1):
+for i in range(m1):
     first += months[i]
 
-for i in  range(m2 + 1):
+for i in  range(m2):
     second += months[i]
 
 answer = second - first
-answer = 1 if answer == 0 else answer
 
 print(answer)
