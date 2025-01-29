915c4c905907df84d0a7a0141e0657b6209f13ff
SelianU <Selian2341@gmail.com>
2025-01-29 09:22:27 +0900

    Title: 그 요일은, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250129/그 요일은/the-day-of-the-day.py
+++ 250129/그 요일은/the-day-of-the-day.py
@@ -4,11 +4,13 @@ A = input()
 # Write your code here!
 
 months = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-day_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+day_of_week = {"Mon": 0, "Tue": 1, "Wed": 2, "Thu": 3, "Fri": 4, "Sat": 5, "Sun": 6}
 
 day1 = d1
 for i in range(m1):
     day1 += months[i]
+
+day1 += day_of_week[A]
 
 day2 = d2
 for i in range(m2):
