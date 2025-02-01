78da3c3626ad3b2cf483039feec822cf0ae31b2c
SelianU <Selian2341@gmail.com>
2025-02-01 20:26:45 +0900

    Title: 왔다 갔던 구역 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250201/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,26 @@
+n = int(input())
+x = []
+dir = []
+for _ in range(n):
+    xi, di = input().split()
+    x.append(int(xi))
+    dir.append(di)
+
+# Write your code here!
+dots = [0 for _ in range(201)]
+
+k = 101
+
+for i in range(n):
+    for j in range(x[i]):
+        dots[k] += 1
+        if dir[i] == 'L':
+            k -= 1
+        if dir[i] == 'R':
+            k += 1
+
+answer = 0
+for dot in dots:
+    if dot > 1:
+        answer += 1
+print(answer)
