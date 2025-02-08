5e94e2f8320492c97b467e76281f62bd284169b9
SelianU <Selian2341@gmail.com>
2025-02-09 00:13:50 +0900

    Title: 벌금은 누구에게, Time: 79ms, Memory: 18MB, Status: Wrong Answer - CodeTree

---
+++ 250209/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,20 @@
+N, M, K = map(int, input().split())
+student = [int(input()) for _ in range(M)]
+
+# Write your code here!
+students = {}
+
+for i in range(M):
+    if student[i] not in students.keys():
+        students[student[i]] = 1
+    else:
+        students[student[i]] += 1
+
+answer = -1
+
+for i in range(1, N):
+    if i in students.keys() and students[i] >= K:
+        answer = i
+        break
+
+print(answer)
