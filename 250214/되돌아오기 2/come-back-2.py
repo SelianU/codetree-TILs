ad06a5902e8610df64a61dd08c4f5f3b91d0e2ae
SelianU <Selian2341@gmail.com>
2025-02-14 15:59:35 +0900

    Title: 되돌아오기 2, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250214/되돌아오기 2/come-back-2.py
@@ -0,0 +1,26 @@
+commands = input()
+
+# Write your code here!
+x, y = 0, 0
+
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+
+dir = 0
+
+count = 0
+
+for command in commands:
+    if command == 'F':
+        x, y = x + dx[dir], y + dy[dir]
+    elif command == 'R':
+        dir = (dir + 1) % 4
+    else:
+        dir = (dir - 1) % 4
+    count += 1
+
+    if x == 0 and y == 0:
+        break
+    
+if count == len(commands):count = -1
+
+print(count)
