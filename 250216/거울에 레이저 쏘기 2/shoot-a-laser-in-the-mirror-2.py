91109f4ec4b61281c0361e0db38999f1e2d4f309
SelianU <Selian2341@gmail.com>
2025-02-16 15:50:57 +0900

    Title: 거울에 레이저 쏘기 2, Time: 157ms, Memory: 49MB, Status: Runtime Error - CodeTree

---
+++ 250216/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -0,0 +1,55 @@
+n = int(input())
+grid = [list(input()) for _ in range(n)]
+k = int(input())
+
+# Write your code here!
+# 어디서 어디로
+def is_reflect(direction, mirror):
+    reflect = 0
+    if mirror == '/':
+        reflect = 3 - direction
+    elif mirror == '\\':
+        if direction < 2:
+            reflect = 1 - direction
+        else:
+            reflect = 5 - direction
+    return reflect
+
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+x, y = 0, 0
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+
+direction = k // n
+
+# 초기 위치 설정
+if direction == 0:
+    x = k - 1
+    y = 0
+elif direction == 1:
+    x = n - 1
+    y = k - n - 1
+elif direction == 2:
+    x = 0 if k % n == 0 else n - k % 3
+    y = n - 1
+else:
+    x = 0
+    y = 0 if k % n == 0 else n - k  % 3
+
+count = 0
+
+while True:
+    direction = is_reflect(direction, grid[y][x])
+    count += 1
+    nx, ny = x + dx[direction], y + dy[direction]
+    if in_range(nx, ny):
+        x, y = x + dx[direction], y + dy[direction]
+        if direction % 2:
+            direction = 2 - direction
+        else:
+            direction = 4 - direction
+    else:
+        break
+
+print(count)
