2cf7d5ca0e7f4340bbae4a36eef8181342729a97
SelianU <Selian2341@gmail.com>
2025-02-17 12:16:50 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 5102ms, Memory: 17MB, Status: Time Limit Exceed - CodeTree

---
+++ 250217/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
@@ -0,0 +1,28 @@
+N, M = map(int, input().split())
+
+grid = [[0] * M for _ in range(N)]
+
+def in_range(x, y):
+    return 0 <= x and x < M and 0 <= y and y < N
+
+x, y = 0, 0
+
+dx, dy = [0, 1, 0, -1], [-1, 0, 1, 0]
+
+direction = 2
+
+number = 1
+
+while number <= N * M:
+    grid[y][x] = number
+    nx, ny = x + dx[direction], y + dy[direction]
+    if in_range(nx, ny) and grid[ny][nx] == 0:
+        x, y = x + dx[direction], y + dy[direction]
+        number += 1
+    else:
+        direction = (direction - 1) % 4
+    
+for g in grid:
+    for i in g:
+        print(i, end=' ')
+    print()
