16c24a0ed5cbece6e3153f3fdf8455bc295eb502
SelianU <Selian2341@gmail.com>
2025-02-21 20:19:39 +0900

    Title: 모이자, Time: 50ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 250221/모이자/gather.py
@@ -0,0 +1,12 @@
+n = int(input())
+A = list(map(int, input().split()))
+
+# Write your code here!
+min_sum = 10000
+for i in range(n):
+    temp = 0
+    for j in range(n):
+        temp += A[j] * abs(j - i)
+    min_sum = min(min_sum, temp)
+
+print(min_sum)
