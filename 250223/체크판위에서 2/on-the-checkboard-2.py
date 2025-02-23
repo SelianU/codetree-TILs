fad3a40ceeef843a69beb6cc785ea0b0b771552c
SelianU <Selian2341@gmail.com>
2025-02-23 16:49:35 +0900

    Title: 체크판위에서 2, Time: 40ms, Memory: 16MB, Status: Passed - CodeTree

--- 250223/체크판위에서 2/on-the-checkboard-2.py
+++ 250223/체크판위에서 2/on-the-checkboard-2.py
@@ -8,9 +8,9 @@ for i in range(1, R - 1):
     for j in range(1, C - 1):
         for k in range(i + 1, R - 1):
             for l in range(j + 1, C - 1):
-                if grid[0][0] == 'W' and grid[i][j] == 'B' and grid[k][l] == 'W':
+                if grid[0][0] == 'W' and grid[i][j] == 'B' and grid[k][l] == 'W' and grid[-1][-1] == 'B':
                     count += 1
-                elif grid[0][0] == 'B' and grid[i][j] == 'W' and grid[k][l] == 'B':
+                elif grid[0][0] == 'B' and grid[i][j] == 'W' and grid[k][l] == 'B' and grid[-1][-1] == 'W':
                     count += 1
 
 print(count)
