b3d59fbdbe4a9b77f86824b8a93b71f640d5f854
SelianU <Selian2341@gmail.com>
2025-02-24 13:45:45 +0900

    Title: 이상한 진수 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250224/이상한 진수 2/awkward-digits-2.py
@@ -0,0 +1,27 @@
+b = input()
+
+bi = int(b)
+
+binarys = []
+
+def to_digit(binary):
+    re = 0
+    i = 0
+    while binary != 0:
+        re += 2 ** i * (binary % 10)
+        binary //= 10
+        i += 1
+    return re
+
+for i in range(len(b)):
+    if b[i] == '0':
+        bi += 10 ** (len(b) - i - 1)
+        binarys.append(bi)
+        bi -= 10 ** (len(b) - i - 1)
+
+max_num = 0
+
+for binary in binarys:
+    max_num = max(max_num, to_digit(binary))
+
+print(max_num)
