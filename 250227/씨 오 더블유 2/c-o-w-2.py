098b29d9adea923cf399253801a945228a170dda
SelianU <Selian2341@gmail.com>
2025-02-27 13:45:28 +0900

    Title: 씨 오 더블유 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250227/씨 오 더블유 2/c-o-w-2.py
@@ -0,0 +1,14 @@
+N = int(input())
+cow = input()
+
+c = o = answer = 0
+
+for s in cow:
+    if s == 'C':
+        c += 1
+    elif s == 'O':
+        o += 1
+    else:
+        answer += c * o
+
+print(answer)
