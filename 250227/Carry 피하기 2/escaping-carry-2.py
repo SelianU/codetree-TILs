1cc3922b12e897da5599eb629049b3cff40f850d
SelianU <Selian2341@gmail.com>
2025-02-27 14:29:54 +0900

    Title: Carry 피하기 2, Time: 50ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250227/Carry 피하기 2/escaping-carry-2.py
@@ -0,0 +1,27 @@
+N = int(input())
+numbers = [input() for _ in range(N)]
+
+non_carry_numbers = 0
+count = 0
+
+def maxlen(num1, num2, num3):
+    return max(len(num1), len(num2), len(num3))
+
+for i in range(N):
+    for j in range(i + 1, N):
+        for k in range(j + 1, N):
+            m = maxlen(numbers[i], numbers[j], numbers[k])
+            num1, num2, num3 = map(int, (numbers[i], numbers[j], numbers[k]))
+            p = 0
+            for power in range(m):
+                p += 1
+                if (num1 // (10 ** power)) % 10 + (num2 // (10 ** power)) % 10 + (num3 // (10 ** power)) % 10 > 9:
+                    break
+            if p == m:
+                non_carry_numbers = max(non_carry_numbers, num1 + num2 + num3)
+                count += 1
+
+if count == N * (N - 1) * (N - 2):
+    print(-1)
+else:
+    print(non_carry_numbers)
