ed9dfcf4fc7769fcc916d7c03572ba2d40fc3a45
SelianU <Selian2341@gmail.com>
2025-02-28 15:09:11 +0900

    Title: 오목, Time: 48ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250228/오목/O-mok.py
+++ 250228/오목/O-mok.py
@@ -2,66 +2,66 @@ board = [list(map(int, input().split())) for _ in range(19)]
 
 # Please write your code here.
 b_win = w_win = False
-b_x = b_y = w_x = w_y = 0
+ans_x = ans_y = 0
 
-def check_row(board, color):
-    win = False
-    x = y = 0
-    for j in range(len(board)):
-        count = 0
-        for i in range(len(board[j]) - 5):
-            if board[j][i] == color:
-                count += 1
-            else:
-                count = 0
-            if count == 5:
-                x = i - 2
-                y = j + 2
-                win = True
-                break
-        if win:
+dx = [-2, -1, 0, 1, 2]
+dy = [-2, -1, 0, 1, 2]
+
+for y_spot in range(2, 17):
+    for x_spot in range(2, 17):
+        if board[y_spot][x_spot] == 1:
+            for i in range(5):
+                if board[y_spot + dy[i]][x_spot + dx[i]] != 1:
+                    break
+                if i == 4:
+                    b_win = True
+            for i in range(5):
+                if board[y_spot + dy[i]][x_spot - dx[i]] != 1:
+                    break
+                if i == 4:
+                    b_win = True
+            for i in range(5):
+                if board[y_spot][x_spot + dx[i]] != 1:
+                    break
+                if i == 4:
+                    b_win = True
+            for i in range(5):
+                if board[y_spot + dy[i]][x_spot] != 1:
+                    break
+                if i == 4:
+                    b_win = True
+        elif board[y_spot][x_spot] == 2:
+            for i in range(5):
+                if board[y_spot + dy[i]][x_spot + dx[i]] != 2:
+                    break
+                if i == 4:
+                    w_win = True
+            for i in range(5):
+                if board[y_spot + dy[i]][x_spot - dx[i]] != 2:
+                    break
+                if i == 4:
+                    w_win = True
+            for i in range(5):
+                if board[y_spot][x_spot + dx[i]] != 2:
+                    break
+                if i == 4:
+                    w_win = True
+            for i in range(5):
+                if board[y_spot + dy[i]][x_spot] != 2:
+                    break
+                if i == 4:
+                    w_win = True
+        if b_win or w_win:
+            ans_x, ans_y = x_spot + 1, y_spot + 1
             break
-    return win, x, y
+    if b_win or w_win:
+        break
 
-def check_column(board, color):
-    win = False
-    x = y = 0
-    for i in range(len(board)):
-        count = 0
-        for j in range(len(board) - 5):
-            if board[i][j] == color:
-                count += 1
-            else:
-                count = 0
-            if count == 5:
-                x = i + 2
-                y = j - 2
-                win=True
-                break
-        if win:
-            break
-    return win, x, y
-
-def check_diagonal(board, color):
-    pass
-
-b_row = check_row(board, 1)
-b_col = check_column(board, 1)
-
-w_row = check_row(board, 2)
-w_col = check_column(board, 2)
-
-if b_row[0]:
+if b_win:
     print(1)
-    print(b_row[1], b_row[2])
-elif b_col[0]:
-    print(1)
-    print(b_col[1], b_col[2])
-elif w_row[0]:
+    print(ans_y, ans_x)
+elif w_win:
     print(2)
-    print(w_row[1], w_row[2])
-elif w_col[0]:
-    print(2)
-    print(w_col[1], w_col[2])
+    print(ans_y, ans_x)
 else:
     print(0)
