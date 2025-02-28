92937d3d0067d5abc3bff4a5e6b5f36c5785c0cc
SelianU <Selian2341@gmail.com>
2025-02-28 15:15:06 +0900

    Title: 오목, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250228/오목/O-mok.py
+++ 250228/오목/O-mok.py
@@ -7,47 +7,50 @@ ans_x = ans_y = 0
 dx = [-2, -1, 0, 1, 2]
 dy = [-2, -1, 0, 1, 2]
 
-for y_spot in range(2, 17):
-    for x_spot in range(2, 17):
+def in_range(x, y):
+    return 0 <= x < 19 and 0 <= y < 19
+
+for y_spot in range(19):
+    for x_spot in range(19):
         if board[y_spot][x_spot] == 1:
             for i in range(5):
-                if board[y_spot + dy[i]][x_spot + dx[i]] != 1:
+                if in_range(x_spot + dx[i], y_spot + dy[i]) and board[y_spot + dy[i]][x_spot + dx[i]] != 1:
                     break
                 if i == 4:
                     b_win = True
             for i in range(5):
-                if board[y_spot + dy[i]][x_spot - dx[i]] != 1:
+                if in_range(x_spot - dx[i], y_spot + dy[i]) and board[y_spot + dy[i]][x_spot - dx[i]] != 1:
                     break
                 if i == 4:
                     b_win = True
             for i in range(5):
-                if board[y_spot][x_spot + dx[i]] != 1:
+                if in_range(x_spot + dx[i], y_spot) and board[y_spot][x_spot + dx[i]] != 1:
                     break
                 if i == 4:
                     b_win = True
             for i in range(5):
-                if board[y_spot + dy[i]][x_spot] != 1:
+                if in_range(x_spot, y_spot + dy[i]) and board[y_spot + dy[i]][x_spot] != 1:
                     break
                 if i == 4:
                     b_win = True
         elif board[y_spot][x_spot] == 2:
             for i in range(5):
-                if board[y_spot + dy[i]][x_spot + dx[i]] != 2:
+                if in_range(x_spot + dx[i], y_spot + dy[i]) and board[y_spot + dy[i]][x_spot + dx[i]] != 2:
                     break
                 if i == 4:
                     w_win = True
             for i in range(5):
-                if board[y_spot + dy[i]][x_spot - dx[i]] != 2:
+                if in_range(x_spot - dx[i], y_spot + dy[i]) and board[y_spot + dy[i]][x_spot - dx[i]] != 2:
                     break
                 if i == 4:
                     w_win = True
             for i in range(5):
-                if board[y_spot][x_spot + dx[i]] != 2:
+                if in_range(x_spot + dx[i], y_spot) and board[y_spot][x_spot + dx[i]] != 2:
                     break
                 if i == 4:
                     w_win = True
             for i in range(5):
-                if board[y_spot + dy[i]][x_spot] != 2:
+                if in_range(x_spot, y_spot + dy[i]) and board[y_spot + dy[i]][x_spot] != 2:
                     break
                 if i == 4:
                     w_win = True
