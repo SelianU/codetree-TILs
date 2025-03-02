e6c0e7214016afe23d1539e8c75ee62d6b7a81af
SelianU <Selian2341@gmail.com>
2025-03-02 20:25:34 +0900

    Title: 최고의 13위치 2, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250302/최고의 13위치 2/best-place-of-13-2.py
+++ 250302/최고의 13위치 2/best-place-of-13-2.py
@@ -2,15 +2,18 @@ n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
-def in_range(x, y):
-    return 0 <= x < n and 0 <= y < n
-
 answer = 0
 
 for i in range(n):
-    for j in range(i + 1, n):
+    for j in range(i, n):
         for ii in range(n - 2):
             for jj in range(n - 2):
-                answer = max(answer, sum(arr[i][ii:ii + 3]) + sum(arr[j][jj:jj + 3]))
+                if i == j:
+                    if n < 6:
+                        break
+                    elif ii + 2 < jj:
+                        answer = max(answer, sum(arr[i][ii:ii + 3]) + sum(arr[j][jj:jj + 3]))
+                else:
+                    answer = max(answer, sum(arr[i][ii:ii + 3]) + sum(arr[j][jj:jj + 3]))
 
 print(answer)
