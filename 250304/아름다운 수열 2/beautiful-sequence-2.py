c338dc6b96b85bb9d3c27413ef29ba4fc414fdc1
SelianU <Selian2341@gmail.com>
2025-03-04 09:11:37 +0900

    Title: 아름다운 수열 2, Time: 49ms, Memory: 16MB, Status: Passed - CodeTree

--- 250304/아름다운 수열 2/beautiful-sequence-2.py
+++ 250304/아름다운 수열 2/beautiful-sequence-2.py
@@ -7,8 +7,10 @@ answer = 0
 
 for i in range(N - M + 1):
     count = 0
+    a = A[i:i + M]
     for b in B:
-        if b in A[i:i + M]:
+        if b in a:
+            a.remove(b)
             count += 1
     if count == M:
         answer += 1
