bf1aab3282d459dde8a14f5d8f79f22e9a56ac4c
SelianU <Selian2341@gmail.com>
2025-03-04 09:25:46 +0900

    Title: 바구니 안의 사탕 2, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 250304/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 250304/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -16,7 +16,13 @@ for idx, p in enumerate(pos):
 l = len(line)
 answer = 0
 
-for k in range(K, l - K):
-    answer = max(answer, sum(line[k - K:k + K + 1]))
+for k in range(l):
+    ad = 0
+    ov = 0
+    if k - K < 0:
+        ad = abs(k - K)
+    if k + K + 1 > l:
+        ov = k + K + 1 - l
+    answer = max(answer, sum(line[k - K + ad:k + K + 1 - ov]))
 
 print(answer)
