ee64d1cf1084a92fa5a1b98fbea2f791197b216a
SelianU <Selian2341@gmail.com>
2025-03-04 09:07:49 +0900

    Title: 아름다운 수열 2, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250304/아름다운 수열 2/beautiful-sequence-2.py
+++ 250304/아름다운 수열 2/beautiful-sequence-2.py
@@ -5,7 +5,7 @@ B = list(map(int, input().split()))
 # Please write your code here.
 answer = 0
 
-for i in range(N - M + 1)
+for i in range(N - M + 1):
     count = 0
     for b in B:
         if b in A[i:i + M]:
