44342443a7bedfc92b17f69f2c20bc16bb948ba2
SelianU <Selian2341@gmail.com>
2025-03-06 10:48:01 +0900

    Title: 개발자의 능력 3, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250306/개발자의 능력 3/ability-of-developer-3.py
+++ 250306/개발자의 능력 3/ability-of-developer-3.py
@@ -6,7 +6,7 @@ answer = float('inf')
 for i in range(6):
     for j in range(6):
         for k in range(6):
-            sum1 = i + j + k
+            sum1 = abilities[i] + abilities[j] + abilities[k]
             sum2 = sum(abilities) - sum1
             answer = min(abs(sum2 - sum1), answer)
 print(answer)
