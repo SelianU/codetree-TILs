120bff79174c2be0fc8f48f6ad20c57db10d6cb5
SelianU <Selian2341@gmail.com>
2025-03-06 10:50:52 +0900

    Title: 개발자의 능력 3, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 250306/개발자의 능력 3/ability-of-developer-3.py
+++ 250306/개발자의 능력 3/ability-of-developer-3.py
@@ -8,6 +8,5 @@ for i in range(6):
         for k in range(j + 1, 6):
             sum1 = abilities[i] + abilities[j] + abilities[k]
             sum2 = sum(abilities) - sum1
-            print(sum1)
             answer = min(abs(sum2 - sum1), answer)
 print(answer)
