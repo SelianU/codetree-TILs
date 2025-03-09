d350c2e925ca60f86c75d516b8071dc51bcdde82
SelianU <Selian2341@gmail.com>
2025-03-09 12:45:35 +0900

    Title: 개발팀의 능력, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250309/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -0,0 +1,17 @@
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+min_difference = float('inf')
+
+for i in range(5):
+    team1 = arr[i]
+    for j in range(5):
+        if i != j:
+            for k in range(5):
+                if i != k and j != k and k != i:
+                    team2 = arr[j] + arr[k]
+                    team3 = sum(arr) - team1 - team2
+                    if team1 != team2 and team2 != team3 and team3 != team1:
+                        min_difference = min(min_difference, max(team1, team2, team3) - min(team1, team2, team3))
+
+print(min_difference)
