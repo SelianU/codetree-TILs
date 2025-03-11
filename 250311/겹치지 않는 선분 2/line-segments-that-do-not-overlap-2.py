3251274103056819915f00fb40bc9dbaa4887b2c
SelianU <Selian2341@gmail.com>
2025-03-11 09:51:47 +0900

    Title: 겹치지 않는 선분 2, Time: 52ms, Memory: 16MB, Status: Passed - CodeTree

--- 250311/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 250311/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -2,15 +2,18 @@ n = int(input())
 lines = [tuple(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
-answer = 0
+count = c = 0
+
+number = []
+
+lines.sort(key=lambda x: x[0])
 
 for i in range(n):
-    count = 0
     for j in range(i + 1, n):
-        if lines[i][0] < lines[j][0] and lines[i][1] < lines[j][1]: # not cross
-            continue
-        count += 1
-    if count == 0:
-        answer += 1
+        if lines[i][1] > lines[j][1]:
+            number.append(i)
+            number.append(j)     
 
-print(answer)
+ns = set(number)
+
+print(n - len(ns))
