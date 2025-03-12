c4346e3e55b82a30f177db16ea8edf89810909eb
SelianU <Selian2341@gmail.com>
2025-03-12 09:27:22 +0900

    Title: 스승의 은혜 2, Time: 180ms, Memory: 23MB, Status: Passed - CodeTree

--- 250312/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 250312/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -5,10 +5,10 @@ P = [int(input()) for _ in range(N)]
 P.sort()
 answer = 0
 
-for i in range(N): # 반값
-    for j in range(N):
+for i in range(N + 1): # 반값
+    for j in range(i + 1, N + 1):
         P_temp = P[:i] + [P[i] // 2] + P[i + 1:j]
-        if sum(P_temp) < B:
+        if sum(P_temp) <= B:
             answer = max(answer, j)
         else:
             continue
