0ef561b4efa3608e69174de585db0539459e4189
SelianU <Selian2341@gmail.com>
2025-03-13 17:01:48 +0900

    Title: 이상한 폭탄 2, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250313/이상한 폭탄 2/strange-bomb-2.py
+++ 250313/이상한 폭탄 2/strange-bomb-2.py
@@ -4,10 +4,14 @@ num = [int(input()) for _ in range(N)]
 # Please write your code here.
 answer = []
 
-for i in range(N - 1, K - 2, -1):
-    for j in range(i - 1, i - K - 2, -1):
-        if num[j] == num[i]:
+for i in range(N - K):
+    for j in range(i + 1, i + K + 1):
+        if num[i] == num[j]:
             answer.append(num[i])
+# for i in range(N - 1, K - 2, -1):
+#     for j in range(i - 1, i - K - 2, -1):
+#         if num[j] == num[i]:
+#             answer.append(num[i])
 
 if not answer:
     print(-1)
