4e47761ed7396a5dfe7b8632e4d736c830dff893
SelianU <Selian2341@gmail.com>
2025-03-14 23:05:10 +0900

    Title: 스승의 은혜 3, Time: 40ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250314/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -0,0 +1,17 @@
+N, B = map(int, input().split())
+gifts = [tuple(map(int, input().split())) for _ in range(N)]
+
+# Please write your code here.
+answer = 0
+for i in range(N):
+    new_gifts = gifts[:i] + [(gifts[i][0] // 2, gifts[i][1])] + gifts[i + 1:]
+    new_new_gifts = [sum(gift) for gift in new_gifts]
+    new_new_gifts.sort()
+    total = 0
+    for j in range(len(new_new_gifts)):
+        total += new_new_gifts[j]
+        if total > B:
+            answer = j + 1
+            break
+
+print(j)
