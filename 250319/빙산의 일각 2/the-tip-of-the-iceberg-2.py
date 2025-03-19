5907a41507f89d96bb72f2698c6ee55069e17130
SelianU <Selian2341@gmail.com>
2025-03-19 13:37:50 +0900

    Title: 빙산의 일각 2, Time: 61ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 250319/빙산의 일각 2/the-tip-of-the-iceberg-2.py
@@ -0,0 +1,17 @@
+n = int(input())
+h = [int(input()) for _ in range(n)]
+
+# Please write your code here.
+# 해수면의 높이를 하나씩 높이면서 덩어리의 수를 구해 answer에 넣는다
+height = max(h) - 1
+answer = 0
+
+for i in range(height):
+    h = [ice - 1 if ice > 1 else 0 for ice in h]
+    count = 0
+    for idx in range(len(h) - 1):
+        if h[idx] == 0 and h[idx + 1] != 0:
+            count += 1
+    answer = max(count, answer)
+
+print(answer + 1)
