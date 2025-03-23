2e69065ed226cbaac333b1f91e62ad7605bbedf8
SelianU <Selian2341@gmail.com>
2025-03-23 22:00:54 +0900

    Title: 좌표평면 위의 균형 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250323/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.py
+++ 250323/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.py
@@ -5,45 +5,37 @@ points = [tuple(map(int, input().split())) for _ in range(n)]
 def divide_x(points):
     points.sort(key = lambda x: x[0])
     length = len(points)
-    if points[length // 2 - 1][0] != points[length // 2][0]:
-        return points[length // 2][0] - 1
-    elif points[length // 2 - 1][0] == points[length // 2][0] and points[length // 2][0] != points[length // 2 + 1][0]:
-        return points[length // 2][0] + 1
+    mid = length // 2
+    if points[mid - 1][0] != points[mid][0]:
+        return points[mid][0] - 1
+    elif points[mid - 1][0] == points[mid][0] and points[mid][0] != points[mid + 1][0]:
+        return points[mid][0] + 1
+    else:
+        return points[mid][0] - 1
     
 def divide_y(points):
     points.sort(key = lambda y: y[1])
     length = len(points)
-    if points[length // 2 - 1][1] != points[length // 2][1]:
-        return points[length // 2][1] - 1
-    elif points[length // 2 - 1][1] == points[length // 2][1] and points[length // 2][1] != points[length // 2 + 1][1]:
-        return points[length // 2][1] + 1
+    mid = length // 2
+    if points[mid - 1][1] != points[mid][1]:
+        return points[mid][1] - 1
+    elif points[mid - 1][1] == points[mid][1] and points[mid][1] != points[mid + 1][1]:
+        return points[mid][1] + 1
+    else:
+        return points[mid][0] - 1
 
 def count_direction(x, y):
     count = [0, 0, 0, 0]
     for point in points:
-        if check_first(x, y, point):
+        if point[0] < x and point[1] > y:
             count[0] += 1
-        if check_second(x, y, point):
+        if point[0] > x and point[1] > y:
             count[1] += 1
-        if check_third(x, y, point):
+        if point[0] > x and point[1] < y:
             count[2] += 1
-        if check_fourth(x, y, point):
+        if point[0] < x and point[1] < y:
             count[3] += 1
-
     return max(count)
-
-def check_first(x, y, point):
-    if point[0] < x and point[1] > y:
-        return True
-def check_second(x, y, point):
-    if point[0] > x and point[1] > y:
-        return True
-def check_third(x, y, point):
-    if point[0] > x and point[1] < y:
-        return True
-def check_fourth(x, y, point):
-    if point[0] < x and point[1] < y:
-        return True
 
 if __name__ == '__main__':
     x = divide_x(points)
