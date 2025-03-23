8504af75a6da8f5be73ddd49064e3c17f4dd0037
SelianU <Selian2341@gmail.com>
2025-03-23 22:21:08 +0900

    Title: 좌표평면 위의 균형 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250323/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.py
+++ 250323/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.py
@@ -1,44 +1,34 @@
 n = int(input())
 points = [tuple(map(int, input().split())) for _ in range(n)]
 
-# Please write your code here.
-def divide_x(points):
-    points.sort(key = lambda x: x[0])
+def divide(points, axis):
+    points.sort(key = lambda x: x[axis])
     length = len(points)
     mid = length // 2
-    if points[mid - 1][0] != points[mid][0]:
-        return points[mid][0] - 1
-    elif points[mid - 1][0] == points[mid][0] and points[mid][0] != points[mid + 1][0]:
-        return points[mid][0] + 1
-    else:
-        return points[mid][0] - 1
-    
-def divide_y(points):
-    points.sort(key = lambda y: y[1])
-    length = len(points)
-    mid = length // 2
-    if points[mid - 1][1] != points[mid][1]:
-        return points[mid][1] - 1
-    elif points[mid - 1][1] == points[mid][1] and points[mid][1] != points[mid + 1][1]:
-        return points[mid][1] + 1
-    else:
-        return points[mid][1] - 1
 
+    if mid > 0 and points[mid - 1][axis] != points[mid][axis]:
+        return (points[mid - 1][axis] + points[mid][axis]) // 2
+    elif mid < length - 1 and points[mid][axis] != points[mid + 1][axis]:
+        return (points[mid][axis] + points[mid + 1][axis]) // 2
+    elif len(set(point[axis] for point in points)) == 1:
+        return points[mid][axis] - 1
+    return points[mid][axis]
+    
 def count_direction(x, y):
     count = [0, 0, 0, 0]
     for point in points:
         if point[0] < x and point[1] > y:
             count[0] += 1
-        if point[0] > x and point[1] > y:
+        elif point[0] > x and point[1] > y:
             count[1] += 1
-        if point[0] > x and point[1] < y:
+        elif point[0] > x and point[1] < y:
             count[2] += 1
-        if point[0] < x and point[1] < y:
+        elif point[0] < x and point[1] < y:
             count[3] += 1
     return max(count)
 
 if __name__ == '__main__':
-    x = divide_x(points)
-    y = divide_y(points)
+    x = divide(points, 0)
+    y = divide(points, 1)
     count = count_direction(x, y)
     print(count)
