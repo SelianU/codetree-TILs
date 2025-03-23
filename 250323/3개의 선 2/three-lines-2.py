a82bc0f7932c3750602acda1fe808f5979e51fa7
SelianU <Selian2341@gmail.com>
2025-03-23 21:10:17 +0900

    Title: 3개의 선 2, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250323/3개의 선 2/three-lines-2.py
+++ 250323/3개의 선 2/three-lines-2.py
@@ -9,9 +9,9 @@ def choose_3_and_delete_column(p_x, points):
                 # p에 있는 거 제외하고 남은거 묶어서 리스트로 만들어
                 p = [point for point in points if point[0] != first and point[0] != second and point[0] != third]
                 if not p:
-                    continue
-                else:
                     return True
+                else:
+                    continue
     return False
 
 def choose_3_and_delete_row(p_y, points):
@@ -21,9 +21,9 @@ def choose_3_and_delete_row(p_y, points):
                 # p에 있는 거 제외하고 남은거 묶어서 리스트로 만들어
                 p = [point for point in points if point[1] != first and point[1] != second and point[1] != third]
                 if not p:
-                    continue
-                else:
                     return True
+                else:
+                    continue
     return False
 
 def choose_2_1_and_delete(p_x, p_y, points):
@@ -33,9 +33,9 @@ def choose_2_1_and_delete(p_x, p_y, points):
                 # p에 있는 거 제외하고 남은거 묶어서 리스트로 만들어
                 p = [point for point in points if point[0] != first and point[0] != second and point[1] != third]
                 if not p:
-                    continue
-                else:
                     return True
+                else:
+                    continue
     return False
 
 def choose_1_2_and_delete(p_x, p_y, points):
@@ -45,9 +45,9 @@ def choose_1_2_and_delete(p_x, p_y, points):
                 # p에 있는 거 제외하고 남은거 묶어서 리스트로 만들어
                 p = [point for point in points if point[0] != first and point[1] != second and point[1] != third]
                 if not p:
-                    continue
-                else:
                     return True
+                else:
+                    continue
     return False
 
 
