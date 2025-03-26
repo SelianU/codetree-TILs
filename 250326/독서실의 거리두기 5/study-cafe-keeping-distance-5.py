c6cd4a8be7d7cea6c28172684160c91a6b0997ab
SelianU <Selian2341@gmail.com>
2025-03-26 10:02:45 +0900

    Title: 독서실의 거리두기 5, Time: 40ms, Memory: 16MB, Status: Passed - CodeTree

--- 250326/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 250326/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -2,31 +2,19 @@ N = int(input())
 seat = input()
 
 # Please write your code here.
-max_blank = 0
-min_blank = float('inf')
-
-count = 0
-for s in range(len(seat)):
-    if seat[s] == '0':
-        count += 1
-    else:
-        if s != 0:
-            min_blank = min(min_blank, count)
-        count = 0
-    max_blank = max(max_blank, count)
-    
-if count != 0:
-    last_blank = count
-
-
 answer = 0
-if max_blank % 2 == 1:
-    answer = max_blank // 2 + 1
-else:
-    answer = max_blank // 2
-
-answer = max(answer, last_blank)
-
-answer = min(answer, min_blank + 1)
+for i in range(len(seat)):
+    if seat[i] == '1':
+        continue
+    else:
+        s = seat[:i] + '1' + seat[i + 1:]
+        pos = []
+        for idx, a in enumerate(s):
+            if a == '1':
+                pos.append(idx)
+        t = []
+        for j in range(len(pos) - 1):
+            t.append(pos[j + 1] - pos[j])
+        answer = max(min(t), answer)
 
 print(answer)
