7da85ad210f34afb74de4c8ad8dd37eebf3358fd
SelianU <Selian2341@gmail.com>
2025-03-26 09:48:51 +0900

    Title: 독서실의 거리두기 5, Time: 77ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250326/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 250326/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -3,14 +3,20 @@ seat = input()
 
 # Please write your code here.
 max_blank = 0
-for s in seat:
-    if s == '0':
+min_blank = float('inf')
+
+for s in range(len(seat)):
+    if seat[s] == '0':
         count += 1
     else:
+        if s != 0:
+            min_blank = min(min_blank, count)
         count = 0
     max_blank = max(max_blank, count)
+    
 if count != 0:
     last_blank = count
+
 
 answer = 0
 if max_blank % 2 == 1:
@@ -19,5 +25,7 @@ else:
     answer = max_blank // 2
 
 answer = max(answer, last_blank)
+
+answer = min(answer, min_blank + 1)
 
 print(answer)
